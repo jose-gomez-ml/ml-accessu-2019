ea3d6988e19efa3954de1273c9918a44fbb42800
José Gómez <jose.gomez@macmillan.com>
2019-05-13 00:47:42 -0500

    cleanup

--- app/appscript.js
+++ app/appscript.js
@@ -14,29 +14,27 @@ var sound;
 var soundLand;
 
 // sound
-  sound = new Howl({
-    src: ['short_whoosh.wav'],
-    autoplay: true,
-    rate: gravityEarth,
-    loop: true,
-    volume: 0.5,
-    onend: function() {
-      console.log('Finished!');
-    }
-  });
+sound = new Howl({
+  src: ['short_whoosh.wav'],
+  autoplay: true,
+  rate: gravityEarth,
+  loop: true,
+  volume: 0.8,
+  onend: function() {
+    console.log('Finished!');
+  }
+});
 
-  soundLand =  new Howl({
-    src: ['woody_click.wav'],
-    autoplay: false,
-    rate: 1,
-    volume: 0.5
-  })
-
-// var planeta = document.getElementById("hi");
+soundLand =  new Howl({
+  src: ['woody_click.wav'],
+  autoplay: false,
+  rate: 1,
+  volume: 0.8 
+})
 
 function initGame() {
   console.log("initGame");
-  myGamePiece = new component(30, 30, "red", 80, 75);
+  myGamePiece = new component(50, 50, "red", 80, 75);
   myGameArea.init();
 }
 
@@ -45,71 +43,65 @@ function startGame() {
 }
 
 var myGameArea = {
-    canvas : document.createElement("canvas"),
-    // canvas : document.getElementById("planetacanvas"),
-    init : function() {
-      var planeta = document.getElementById("planetcanvascontainer");
-      // this.canvas.width = 480;
-      this.canvas.height = 470;
-      this.context = this.canvas.getContext("2d");
-      console.log(planeta);
-      // document.body.appendChild(this.canvas, document.getElementById("planeta"));
-      planetcanvascontainer.appendChild(this.canvas);
-      updateGameArea();
-    },
-    start : function() {
-        this.interval = setInterval(updateGameArea, 20);        
-    },
-    stop : function() {
-        clearInterval(this.interval);
-        sound.stop();
-        // soundLand.stop();
-    },    
-    clear : function() {
-        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
-    }
+  canvas : document.createElement("canvas"),
+  init : function() {
+    var planeta = document.getElementById("planetcanvascontainer");
+    // this.canvas.width = 480;
+    this.canvas.height = 470;
+    this.context = this.canvas.getContext("2d");
+    console.log(planeta);
+    planetcanvascontainer.appendChild(this.canvas);
+    updateGameArea();
+  },
+  start : function() {
+      this.interval = setInterval(updateGameArea, 20);        
+  },
+  stop : function() {
+      clearInterval(this.interval);
+      sound.stop();
+  },    
+  clear : function() {
+      this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+  }
 }
 
 function component(width, height, color, x, y, type) {
-    console.log("this" , planetgravity);
-    this.type = type;
-    this.width = width;
-    this.height = height;
-    this.x = x;
-    this.y = y;    
-    this.speedX = 0;
-    this.speedY = 0;    
-    this.gravity = planetgravity;
-    this.gravitySpeed = 0;
-    this.update = function() {
-        ctx = myGameArea.context;
-        ctx.fillStyle = color;
-        ctx.fillRect(this.x, this.y, this.width, this.height);
-    }
-    this.newPos = function() {
-        this.gravitySpeed += planetgravity;
-        this.x += this.speedX;
-        this.y += this.speedY + this.gravitySpeed;
-        this.hitBottom();
-    }
-    this.hitBottom = function() {
-        var rockbottom = myGameArea.canvas.height - this.height;
-        if (this.y > rockbottom) {
-            this.y = rockbottom;
-            sound.stop();
-            soundLand.play();
-            myGameArea.stop();
-        }
-        
-    }
+  console.log("this" , this);
+  this.type = type;
+  this.width = width;
+  this.height = height;
+  this.x = x;
+  this.y = y;    
+  this.speedX = 0;
+  this.speedY = 0;    
+  this.gravity = planetgravity;
+  this.gravitySpeed = 0;
+  this.update = function() {
+      ctx = myGameArea.context;
+      ctx.fillStyle = color;
+      ctx.fillRect(this.x, this.y, this.width, this.height);
+  }
+  this.newPos = function() {
+      this.gravitySpeed += planetgravity;
+      this.x += this.speedX;
+      this.y += this.speedY + this.gravitySpeed;
+      this.hitBottom();
+  }
+  this.hitBottom = function() {
+      var rockbottom = myGameArea.canvas.height - this.height;
+      if (this.y > rockbottom) {
+          this.y = rockbottom;
+          sound.stop();
+          soundLand.play();
+          myGameArea.stop();
+      }
+  }
 }
 
 function updateGameArea() {
-    myGameArea.clear();
-    myGamePiece.newPos();
-    myGamePiece.update();
-    // sound.rate = 3;
-    // console.log("sdoun", sound.rate);
+  myGameArea.clear();
+  myGamePiece.newPos();
+  myGamePiece.update();
 }
 
 function planetaChange(e){
@@ -133,12 +125,8 @@ function planetaChange(e){
     autoplay: false,
     rate: audioRate,
     loop: true,
-    volume: 0.5,
-    // onend: function() {
-    //   console.log('Finished!');
-    // }
+    volume: 0.8,
   });
-  // sound.rate = planetgravity;
 }
 
 function resetGame(){
@@ -151,8 +139,5 @@ function resetGame(){
 function dropthemic() {
   console.log("dropthemic");
   myGameArea.start();
-  // sound.play('first');
   sound.play();
 }
-
-// startGame();
